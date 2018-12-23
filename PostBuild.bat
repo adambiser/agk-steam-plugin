c0a6b0fb592ea2d1e7c0dc7ee2b7309567c1f882
Adam Biser <adambiser@gmail.com>
2018-12-23 02:46:42 -0500

    PostBuild.bat improvements
    Finds AGK path from registry using AGK file type association.
    Compiles all example projects and reports failures.

--- PostBuild.bat
+++ PostBuild.bat
@@ -1,12 +1,55 @@
 @ECHO OFF
-Copy "%1" "..\..\AGKPlugin\SteamPlugin\Windows.dll"
-cd ..
-cd ..
-IF EXIST "UpdateAGKFolder.bat" (
-	CALL UpdateAGKFolder.bat
+REM This is needed for errorlevel to work in the for loop, also need ! instead of %.
+setlocal EnableDelayedExpansion
+
+REM Must be run from the batch file's path.
+cd /d %~dp0
+
+REM set PLUGIN_FILE=AGKPlugin\SteamPlugin\Windows.dll
+REM SET AGK_PATH=E:\SteamApps\steamapps\common\App Game Kit 2\Tier 1\Compiler
+
+REM Determine the path to the compiler.
+REM Start by finding the path to the AGK editor using the AGK File type association.
+REM This might not work in some setups.
+set KEY_NAME="HKCU\Software\Classes\agk_auto_file\shell\open\command"
+for /F "usebackq skip=2 tokens=2,*" %%A in (`REG QUERY %KEY_NAME% /ve`) do (
+	set AGK_PATH=%%B
+)
+if defined AGK_PATH (
+	REM Registry value looks like this: '"PATH_TO_AGK.EXE" "%1"'
+	REM Remove ' "%1"' from the end.
+	set AGK_PATH=!AGK_PATH: "%%1"=!
+	REM Remove the editor EXE portion of the path.
+	set AGK_PATH=!AGK_PATH:\editor\bin\agk.exe=!
+	REM Remove quotes
+	set AGK_PATH=!AGK_PATH:"=!
+	REM Add the path to the compiler.
+	set AGK_COMPILER_PATH=!AGK_PATH!\Compiler\AGKCompiler.exe
+	set AGK_PLUGIN_PATH=!AGK_PATH!\Compiler\Plugins\SteamPlugin
+)
+REM Verify compiler path.
+if not defined AGK_COMPILER_PATH set NO_COMPILER=1
+if not exist !AGK_COMPILER_PATH! set NO_COMPILER=1
+if defined NO_COMPILER (
+	echo ERROR: Could not determine path to AGKCompiler.exe. Is the AGK file type associated with AGK.EXE?
+	exit /B 1
+)
+echo Compiler path: !AGK_COMPILER_PATH!
+echo Plugin path: !AGK_PLUGIN_PATH!
+
+echo Copying plugin into AppGameKit plugin folder.
+SET AGKPATH=E:\SteamApps\steamapps\common\App Game Kit 2
+COPY "AGKPlugin\SteamPlugin" "%AGK_PLUGIN_PATH%" > nul
+
+echo Compiling example projects.
+for /D %%G in ("Examples\*") do (
+	REM copy %PLUGIN_FILE% "%%G\Plugins\SteamPlugin\Windows.dll" > nul
+	REM if !errorlevel! neq 0 echo ERROR: "%%G" plugin copy failed!
+	echo    %%G
+	pushd "%%G"
+	"%AGK_COMPILER_PATH%" -agk "main.agc"
+	if !errorlevel! neq 0 echo ERROR: "%%G" compilation failed!
+	popd
 )
 
-@ECHO Copy to each example project.
-FOR /D %%G in ("Examples\*") DO (
-	Copy "%1" "%%G\Plugins\SteamPlugin\Windows.dll"
-)
+exit /B 0
