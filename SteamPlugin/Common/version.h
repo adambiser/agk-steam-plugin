f8435aa68687fefba60b257252c63078f3013bb0
Adam Biser <adambiser@gmail.com>
2023-09-06 01:43:58 -0400

    Compiled 1.11

--- SteamPlugin/Common/version.h
+++ SteamPlugin/Common/version.h
@@ -6,7 +6,7 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR				1
-#define VERSION_MINOR				57
+#define VERSION_MINOR				11
 #define VERSION_REVISION			0
 #define VERSION_BUILD				0
 
